0999412cfd9486be524a274ce1669698337b7cfe
unknown <archcores@gmail.com>
2021-01-28 22:38:45 +0500

    'complete'

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/steve.jpg'),
+              ),
+              Text(
+                'Steve Zissou',
+                style: TextStyle(
+                  fontFamily: 'FredokaOne',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'OCEANOGRAPHER',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              SizedBox(
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+                height: 20.0,
+                width: 150.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                    size: 20.0,
+                  ),
+                  title: Text(
+                    '+1 234 567 89 00',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'zissou@email.com',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
